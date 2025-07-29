2f99eb40093b02523247d22a9e4e710ee14c64de
NathanPip <107423247+NathanPip@users.noreply.github.com>
2025-07-28 22:39:13 -0700

    fix heart and score

--- scripts/score_counter.gd
+++ scripts/score_counter.gd
@@ -1,7 +1,12 @@
 extends RichTextLabel
 
+var is_gameover: bool = false
+
 func change_text(amt: int):
-	self.text = str(amt)
+	if is_gameover:
+		self.text = str(amt) + " Seconds"
+	else:
+		self.text = str(amt)
 
 func _ready() -> void:
 	Globals.count_up.connect(change_text)
