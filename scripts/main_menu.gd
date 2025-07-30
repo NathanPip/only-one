7f743f5dd42bc389eba59f9ee7006dfae3776eb2
NathanPip <107423247+NathanPip@users.noreply.github.com>
2025-07-29 22:45:38 -0700

    fix sounds and change speed of projectiles

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,9 +2,23 @@ extends Control
 
 signal on_ready
 
+@export var screens: Array[Control] = []
+var current_screen = 0
+
+func show_next():
+	current_screen += 1
+	if current_screen > 0:
+		screens[current_screen-1].visible = false
+	screens[current_screen].visible = true
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("action"):
-		Globals.game_state = Globals.GameStateEnum.PLAYING
+		if current_screen < screens.size()-1:
+			show_next()
+		else:
+			Globals.game_state = Globals.GameStateEnum.PLAYING
+			current_screen = -1
+			show_next()
 
 func _ready() -> void:
 	on_ready.emit()
